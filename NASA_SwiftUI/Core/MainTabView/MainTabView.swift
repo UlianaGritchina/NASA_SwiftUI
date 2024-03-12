1ea6b9f395a5e746816ec782b64c7821d7497078
Uliana Gritchina <ugritchina@icloud.com>
2024-03-12 18:38:52 +0300

    Initial project settings.

---
+++ NASA_SwiftUI/Core/MainTabView/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  NASA_SwiftUI
+//
+//  Created by Ульяна Гритчина on 12.03.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainTabView()
+}
--- NASA_SwiftUI/SupportingFiles/NASA_SwiftUIApp.swift
+++ NASA_SwiftUI/SupportingFiles/NASA_SwiftUIApp.swift
@@ -13,7 +13,7 @@ struct NASA_SwiftUIApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
